3983faa03e775bb908fbf45f9ac4fa5afcdca154
PlGGS <bbor0422@gmail.com>
2018-07-02 02:32:52 -0500

    Updated InitGPIO Removed debug button handlers Updated a few gampad related things

--- R2D2G2/R2D2G2/MainPage.xaml.cs
+++ R2D2G2/R2D2G2/MainPage.xaml.cs
@@ -13,16 +13,18 @@ namespace R2D2G2
     public sealed partial class MainPage : Page
     {
         DispatcherTimer timer;
-        Gamepad controller;
+        IReadOnlyList<Gamepad> gamepads;
+        Gamepad gamepad;
         Motor lLeg;
         Motor rLeg;
         Motor head;
+        public int[] pinNums = { 6, 12, 19, 16, 26, 20 };
         public GpioPin[] Pins { get; set; } = new GpioPin[6];
 
         public MainPage()
         {
             this.InitializeComponent();
-            
+
             timer = new DispatcherTimer();
             timer.Interval = TimeSpan.FromMilliseconds(500);
             timer.Tick += Timer_Tick;
@@ -30,7 +32,7 @@ namespace R2D2G2
             InitGPIO();
             InitMotors();
 
-            //IReadOnlyList<Gamepad> gamepads = Gamepad.Gamepads;
+            gamepads = Gamepad.Gamepads;
 
             timer.Start();
         }
@@ -59,43 +61,25 @@ namespace R2D2G2
             //Initialize pins
             txbDebug.Text = $"GPIO pins ";
 
-            Pins[0] = gpio.OpenPin(6);
-            Pins[0].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"0 ";
-            Pins[1] = gpio.OpenPin(12);
-            Pins[1].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"1 ";
-            Pins[2] = gpio.OpenPin(19);
-            Pins[2].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"2 ";
-            Pins[3] = gpio.OpenPin(16);
-            Pins[3].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"3 ";
-            Pins[4] = gpio.OpenPin(26);
-            Pins[4].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"4 ";
-            Pins[5] = gpio.OpenPin(20);
-            Pins[5].SetDriveMode(GpioPinDriveMode.Output);
-            txbDebug.Text += $"5 ";
+            for (int i = 0; i < Pins.Length; i++)
+            {
+                Pins[i] = gpio.OpenPin(pinNums[i]);
+                Pins[i].SetDriveMode(GpioPinDriveMode.Output);
+                txbDebug.Text += $"i ";
+            }
 
             txbDebug.Text += "initialized properly";
-
-            /*  
-                    InitPin(tmpPin, pair, item);
-                txbDebug.Text += $"{pin} ";
-                tmpPin++;
-            */
         }
         
         private void Timer_Tick(object sender, object e)
         {
-            if (Gamepad.Gamepads.Count > 0)
+            if (gamepads.Count > 0)
             {
-                if (Gamepad.Gamepads[0] != null)
+                if (gamepads[0] != null)
                 {
-                    controller = Gamepad.Gamepads[0];
+                    gamepad = Gamepad.Gamepads[0];
                 }
-                var reading = controller.GetCurrentReading();
+                var reading = gamepad.GetCurrentReading();
                 //txbDebug.Text = $"{reading} pressed";
 
                 //pbLeftThumbstickX.Value = reading.LeftThumbstickX;
@@ -103,91 +87,28 @@ namespace R2D2G2
 
                 //https://msdn.microsoft.com/en-us/library/windows/apps/windows.gaming.input.gamepadbuttons.aspx
                 //ChangeVisibility(reading.Buttons.HasFlag(GamepadButtons.A), lblA);
-            }
-        }
-        
-        private void ChangeVisibility(bool flag, UIElement elem)
-        {
-            if (flag)
-            {
-                elem.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                elem.Visibility = Visibility.Collapsed;
+
+                /*if (Pins[0].Read() == GpioPinValue.Low)
+                {
+                    Pins[0].Write(GpioPinValue.High);
+                }
+                else
+                {
+                    Pins[0].Write(GpioPinValue.Low);
+                }*/
             }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (Pins[0].Read() == GpioPinValue.Low)
-            {
-                Pins[0].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[0].Write(GpioPinValue.Low);
-            }
+            Pins[Convert.ToInt32(Name)].Write(GpioPinValue.High);
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Loaded(object sender, RoutedEventArgs e)
         {
-            if (Pins[1].Read() == GpioPinValue.Low)
-            {
-                Pins[1].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[1].Write(GpioPinValue.Low);
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            if (Pins[2].Read() == GpioPinValue.Low)
-            {
-                Pins[2].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[2].Write(GpioPinValue.Low);
-            }
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            if (Pins[3].Read() == GpioPinValue.Low)
-            {
-                Pins[3].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[3].Write(GpioPinValue.Low);
-            }
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
-        {
-            if (Pins[4].Read() == GpioPinValue.Low)
-            {
-                Pins[4].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[4].Write(GpioPinValue.Low);
-            }
-        }
-
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            if (Pins[5].Read() == GpioPinValue.Low)
-            {
-                Pins[5].Write(GpioPinValue.High);
-            }
-            else
-            {
-                Pins[5].Write(GpioPinValue.Low);
-            }
+#if RELEASE
+            this.Visibility = Visibility.Collapsed;
+#endif
         }
     }
 }
