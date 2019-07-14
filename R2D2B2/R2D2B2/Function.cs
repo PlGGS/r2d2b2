c68216b5121dbf035604e9a86ddfd290aee2d6dc
Blake Boris <bbor0422@gmail.com>
2019-07-14 04:19:56 -0500

    Completely refactored alexa skill

--- R2D2B2/R2D2B2/Function.cs
+++ R2D2B2/R2D2B2/Function.cs
@@ -14,82 +14,97 @@ namespace R2D2B2
 {
     public class Function
     {
-        public SkillResponse FunctionHandler(SkillRequest input, ILambdaContext context)
+        SkillResponse response = new SkillResponse();
+        readonly string version = "1.0.0";
+        Type skillRequestType = typeof(LaunchRequest); //Default type to launchRequest bc it will always be that first
+        ILambdaLogger logger;
+
+        public SkillResponse FunctionHandler (SkillRequest input, ILambdaContext context)
         {
-            SkillResponse response = new SkillResponse();
             response.Response = new ResponseBody();
-            response.Response.ShouldEndSession = false;
-            IOutputSpeech innerResponse = null;
-            ILambdaLogger log = context.Logger;
-            string version = "v1.0.0";
+            response.Version = version;
+
+            skillRequestType = input.GetRequestType();
+            logger = context.Logger;
 
             if (input.GetRequestType() == typeof(LaunchRequest))
             {
-                log.LogLine($"Default LaunchRequest made: 'Alexa, run blake bot");
-
-                innerResponse = new PlainTextOutputSpeech();
-                (innerResponse as PlainTextOutputSpeech).Text = $"Beep boop beep bop. I am R2D2 {version}";
+                Speak($"Beep boop beep bop. I am R2D2B2 version {version}");
             }
             else if (input.GetRequestType() == typeof(IntentRequest))
             {
                 IntentRequest intentRequest = (IntentRequest)input.Request;
+
                 switch (intentRequest.Intent.Name)
                 {
                     case "AMAZON.CancelIntent":
-                        log.LogLine($"AMAZON.CancelIntent: send StopMessage");
-                        innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = "Cancelled";
-                        response.Response.ShouldEndSession = false;
+                        Speak("Cancelled", false);
                         break;
                     case "AMAZON.HelpIntent":
-                        log.LogLine($"AMAZON.HelpIntent: send HelpMessage");
-                        innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = "Placeholder help message";
+                        Speak("Placeholder help message");
                         response.Response.ShouldEndSession = false;
                         break;
                     case "AMAZON.StopIntent":
-                        log.LogLine($"AMAZON.StopIntent: send StopMessage");
-                        innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = "Cancelled";
+                        Speak("Cancelled");
                         response.Response.ShouldEndSession = false;
                         break;
                     case "AMAZON.NavigateHomeIntent":
-                        log.LogLine($"AMAZON.NavigateHomeIntent: send default message like: Hey R2");
-                        innerResponse = new SsmlOutputSpeech();
-                        (innerResponse as SsmlOutputSpeech).Ssml = "<audio src=\"https://bblake.info/assets/r2d2b2/probably-not.mp3\"/>"; //TODO fix audio playback
+                        Speak("AMAZON.NavigateHomeIntent: send default message like: Hey R2");
+                        response.Response.ShouldEndSession = false;
+                        break;
+                    case "RememberBryanIntent":
+                        Speak("Hello people of America and possibly other countries! I am not British, but today, I'll be showing you <break time=\"2s\"/> a map." + SsmlAudioFile("https://bblake.info/assets/r2d2b2/never.mp3"));
+                        response.Response.ShouldEndSession = false;
+                        break;
+                    case "RememberJoeIntent":
+                        Speak("I love being called Joey!" + SsmlAudioFile("https://bblake.info/assets/r2d2b2/what-what-what-does-that-even-mean.mp3"));
                         response.Response.ShouldEndSession = false;
                         break;
                     case "RememberPersonIntent":
-                        log.LogLine($"RememberPersonIntent sent");
-                        innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = "What the fuck did you just fucking say about me, you little bitch? I’ll have you know " +
-                "I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on " +
-                "Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top " +
-                "sniper in the entire US armed forces.You are nothing to me but just another target.I will wipe you " +
-                "the fuck out with precision the likes of which has never been seen before on this Earth, mark my " +
-                "fucking words. You think you can get away with saying that shit to me over the Internet ? Think " +
-                "again, fucker.As we speak I am contacting my secret network of spies across the USA and your IP is " +
-                "being traced right now so you better prepare for the storm, maggot. The storm that wipes out the " +
-                "pathetic little thing you call your life.You’re fucking dead, kid.I can be anywhere, anytime, and " +
-                "I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I " +
-                "extensively trained in unarmed combat, but I have access to the entire arsenal of the United " +
-                "States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face " +
-                "of the continent, you little shit.If only you could have known what unholy retribution your little " +
-                "“clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. " +
-                "But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury " +
-                "all over you and you will drown in it. You’re fucking dead, kiddo.";
+                        Speak(  "What the fuck did you just fucking say about me, you little bitch? I’ll have you know " +
+                                "I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on " +
+                                "Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top " +
+                                "sniper in the entire US armed forces.You are nothing to me but just another target.I will wipe you " +
+                                "the fuck out with precision the likes of which has never been seen before on this Earth, mark my " +
+                                "fucking words. You think you can get away with saying that shit to me over the Internet ? Think " +
+                                "again, fucker.As we speak I am contacting my secret network of spies across the USA and your IP is " +
+                                "being traced right now so you better prepare for the storm, maggot. The storm that wipes out the " +
+                                "pathetic little thing you call your life.You’re fucking dead, kid.I can be anywhere, anytime, and " +
+                                "I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I " +
+                                "extensively trained in unarmed combat, but I have access to the entire arsenal of the United " +
+                                "States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face " +
+                                "of the continent, you little shit.If only you could have known what unholy retribution your little " +
+                                "“clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. " +
+                                "But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury " +
+                                "all over you and you will drown in it. You’re fucking dead, kiddo.");
                         break;
                     default:
-                        log.LogLine($"Unknown intent: {intentRequest.Intent.Name}");
-                        innerResponse = new PlainTextOutputSpeech();
-                        (innerResponse as PlainTextOutputSpeech).Text = $"Unknown intent: {intentRequest.Intent.Name}";
+                        Speak($"Unknown intent: {intentRequest.Intent.Name}");
                         break;
                 }
             }
 
-            response.Response.OutputSpeech = innerResponse;
-            response.Version = "1.0";
             return response;
+        }
+
+        void Speak (string ssml, bool log = true, bool endSession = false)
+        {
+            response.Response.ShouldEndSession = endSession;
+
+            if (log)
+            {
+                logger.LogLine($"{skillRequestType.ToString()} at <say-as interpret-as=\"time\">{DateTime.Now}</say-as> P.M");
+            }
+
+            response.Response.OutputSpeech = new SsmlOutputSpeech()
+            {
+                Ssml = $"<speak>{ssml}</speak>"
+            };
+        }
+
+        string SsmlAudioFile (string fileURL)
+        {
+            return $"<audio src=\"{fileURL}\"/>";
         }
     }
 }
